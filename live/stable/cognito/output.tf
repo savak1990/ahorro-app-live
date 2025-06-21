6150eea287513123111cadf79b4ef3931c6c45bb
Viacheslav Klovan <slava.klovan@gmail.com>
2025-06-21 11:43:38 +0200

    Updated output of cognito module

--- live/stable/cognito/output.tf
+++ live/stable/cognito/output.tf
@@ -7,3 +7,8 @@ output "user_pool_client_id" {
   description = "The ID of the Cognito User Pool Client."
   value       = module.cognito.user_pool_client_id
 }
+
+output "user_pool_arn" {
+  description = "The ARN of the Cognito User Pool."
+  value       = module.cognito.user_pool_arn
+}
